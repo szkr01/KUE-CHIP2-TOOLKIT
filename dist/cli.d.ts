e39d80ccbddbfcaab10a1a0418543c001e227e73
szkr01 <suzuki82210@gmail.com>
2026-05-18 12:53:42 +0900

    Remove entire implementation of the KueChip2 emulator, assembler, disassembler, and related utilities from the codebase.

--- dist/cli.d.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-type Io = {
-    stdout: Pick<NodeJS.WriteStream, "write">;
-    stderr: Pick<NodeJS.WriteStream, "write">;
-};
-declare function runCli(argv: string[], io?: Io): number;
-
-export { runCli };
